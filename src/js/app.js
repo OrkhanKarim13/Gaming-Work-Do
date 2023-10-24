9ce5c1dd2f4951e7b12f5d7c26c604cac1f1f675
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2023-10-24 13:17:08 +0400

    Added sections for home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,40 @@
-
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: false,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: true,
-      pauseOnMouseEnter: true,
-    },
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 50,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  effect: "coverflow",
+  grabCursor: false,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: true,
+    pauseOnMouseEnter: true,
+  },
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const headerTop = document.querySelector(".headerTop");
+const headerBottom = document.querySelector(".headerBottom");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    headerBottom.style.display = "none";
+
+    headerTop.style.backgroundColor = "black";
+  } else {
+    headerBottom.style.display = "flex";
+
+    headerTop.style.backgroundColor = "";
+  }
+});
+
+
+
+
